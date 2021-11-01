cdf0cbcb999664265b75f1a0d4d8cdfaf5a9687f
Oshioke Iyogwoya <danieloshos3@gmail.com>
2021-11-01 16:31:22 +0100

    added social links and changed fonts

--- src/containers/DAOandLabsSection/DAOandLabsSection.jsx
+++ src/containers/DAOandLabsSection/DAOandLabsSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SectionPadding from '../../components/SectionPadding/SectionPadding';
+import { DiscordIcon, GithubIcon, MediumIcon, TwitterIcon } from '../../components/svgIcons';
 import './DAOandLabsSection.scss';
 
 const Card = props =>{
@@ -12,8 +13,17 @@ const Card = props =>{
 }
 
 const Button = props =>{
-    const {text} = props;
+    const {text, link} = props;
     return(
+        link ?           
+        <a
+            href={link}
+            rel="noopener noreferrer"
+            target="_blank"
+        >
+            {text}
+        </a>
+        :
         <button>{text}</button>
     )
 }
@@ -35,7 +45,7 @@ const DAOandLabsSection = props =>{
                 <div className = "left-side">
                     <div className = "buttons-container">
                         <Button text = "Whitepaper"/>
-                        <Button text = "Documentation"/>
+                        <Button text = "Documentation" link = "https://docs.honey.finance/"/>
                     </div>                    
                 </div>
 
@@ -43,6 +53,37 @@ const DAOandLabsSection = props =>{
                     title = "Honey Labs"
                     text = "Honey Labs builds the protocol and proposes changes to the DAO. It oversees day to day operations and implements changes to the protocol."
                 />
+            </div>
+            <hr/>
+            <div className = "socials-container">
+            <a
+              href="https://honeylabs.medium.com"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <MediumIcon />
+            </a>
+            <a
+              href="https://twitter.com/honeydefi"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <TwitterIcon />
+            </a>
+            <a
+              href="https://discord.gg/T7RQ8hMamB"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <DiscordIcon />
+            </a>
+            <a
+              href="https://github.com/honey-labs"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <GithubIcon />
+            </a>
             </div>
             </SectionPadding>
         </div>
